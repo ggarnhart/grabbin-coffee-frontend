ffdc54c8f315a8c368bd0760bda44070e8f25c11
Greg Garnhart <gegarnhart@email.wm.edu>
2020-02-25 19:26:40 -0500

    minor bug fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ATTEND, DECLINE } from "./types";
 
 export const attend = info => async dispatch => {
-  const res = await axios.post("https://7bbca91f.ngrok.io/api/attend", info);
+  const res = await axios.post("https://7bbca91f.ngrok.io/api/accept", info);
 
   dispatch({ type: ATTEND, payload: res.data });
 };
